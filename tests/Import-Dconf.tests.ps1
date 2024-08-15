d5dd5b83bb39138714842384f765aacedf6e1c95
Freddie Sackur <freddie.git@dustyfox.uk>
2024-08-15 17:12:27 +0100

    tests for Import-Dconf

---
+++ tests/Import-Dconf.tests.ps1
@@ -0,0 +1,20 @@
+Describe "Import-Dconf" {
+    BeforeAll {
+        Mock -ModuleName dconf dconf {
+            if ($args[0] -ne 'write') {throw "unmocked command"}
+            if (-not $args[1]) {throw "missing dconf path"}
+        }
+
+        "[/]`nlast-folder-path='/foo/bar'" | Import-Dconf -Path "/org/gnome/portal/filechooser/kitty/"
+    }
+
+    It "Writes settings" {
+        Should -Invoke dconf -ModuleName dconf -Scope Describe -ParameterFilter {
+            $args[0] | Should -BeExactly "write"
+            $args[1] | Should -BeExactly "/org/gnome/portal/filechooser/kitty/last-folder-path"
+            $args[2] | Should -BeExactly "'/foo/bar'"
+            $args.Count | Should -Be 3
+            $true
+        }
+    }
+}

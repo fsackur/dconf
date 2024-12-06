384c2afe01e5d848b8128cd5798a0294fcd6381f
Freddie Sackur <freddie.git@dustyfox.uk>
2024-12-06 18:03:38 +0000

    drop error on load when dconf not on path

--- dconf.psm1
+++ dconf.psm1
@@ -1,13 +1,3 @@
-try
-{
-    $null = Get-Command dconf -ErrorAction Stop
-}
-catch
-{
-    $_.ErrorDetails = "Command not found: dconf. Please ensure it is on the path."
-    throw $_
-}
-
 #region build-inlines
 $Folders = "$PSScriptRoot/private", "$PSScriptRoot/public" | Resolve-Path -ea Ignore
 $Folders |

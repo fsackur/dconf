70b453f97b79e5a6d19bd95ae88356d4cddfa4db
Freddie Sackur <freddie.git@dustyfox.uk>
2025-01-09 18:08:43 +0000

    deserialise maybes (wip)

--- provider/GVariant.cs
+++ provider/GVariant.cs
@@ -129,12 +129,20 @@ namespace Dconf
     {
         public GMaybe(GVariant genericArg)
         {
+            GenericArg = genericArg;
             ManagedType = typeof(Maybe<>).MakeGenericType([ genericArg.ManagedType ]);
         }
 
+        private GVariant GenericArg { get; init;}
+
         public Type ManagedType { get; init; }
 
-        public object? Deserialize(string encoded) => throw new NotImplementedException();
+        public object? Deserialize(string encoded)
+        {
+            return encoded == "@ms nothing"
+                ? ManagedType.None
+                : ManagedType.Some(GenericArg.Deserialize(encoded));
+        }
     }
 
     public class GArray : GVariant
